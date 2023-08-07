ad57613b9e9a999ad3f5fb43763558c1bfca5d92
kamil <kamil@LAPTOP-3G4F87TS>
2023-08-07 18:01:31 +0400

    Add project files.

---
+++ 4 paskaita ND/Program.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_paskaita_ND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool toliau = true;
+
+            while (toliau)
+            {
+                Console.WriteLine("Pasirinkite funkciją.");
+                Console.WriteLine("1. Skaičiuotuvas");
+                Console.WriteLine("2. Spėliojimo žaidimas");
+                Console.WriteLine("3. Kauliukas");
+                Console.WriteLine("4. Kavos aparatas");
+                Console.WriteLine("x - baigti programą.");
+                Console.WriteLine();
+                Console.WriteLine("Įveskite tik skaičių arba x.");
+
+                string funNr = Console.ReadLine(); //FUnckijos pasirinkimui
+
+                Console.WriteLine();
+
+                if (funNr == "1")
+                {
+                    Skaiciuotuvas();
+                }
+                else if (funNr == "2")
+                {
+                    SpeliojimoZaidimas();
+                }
+                else if (funNr == "3")
+                {
+                    Kauliukas();
+                }
+                else if (funNr == "4")
+                {
+                    KavosAparatas();
+                }
+                else if (funNr == "x")
+                {
+                    toliau = false;
+                }
+                else
+                {
+                    Console.WriteLine("Neteisinga funkcija.");
+                    Console.WriteLine();
+                }
+            }
+
+        }
+
+        //Funkcija įvykdo pasirinką aritmetinį veiksmą.
+        public static void Skaiciuotuvas()
+        {
+            bool toliau = true;
+
+            while (toliau)
+            {
+                Console.WriteLine("Pasirinkite veiksmą: + - / *");
+                string veiksmas = Console.ReadLine();
+
+                if (veiksmas != "+" && veiksmas != "-" && veiksmas != "/" && veiksmas != "*")
+                {
+                    Console.WriteLine("Neteisingas veiksmas.");
+                }
+                else
+                {
+                    Console.WriteLine("Vieną po kito įveskite du skaičius.");
+                    int sk1 = int.Parse(Console.ReadLine());
+                    int sk2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (veiksmas == "+")
+                    {
+                        Console.WriteLine("{0} + {1} = {2}", sk1, sk2, sk1 + sk2);
+                    }
+                    else if (veiksmas == "-")
+                    {
+                        Console.WriteLine("{0} - {1} = {2}", sk1, sk2, sk1 - sk2);
+                    }
+                    else if (veiksmas == "/")
+                    {
+                        Console.WriteLine("{0} / {1} = {2}", sk1, sk2, (float)sk1 / sk2);
+                    }
+                    else if (veiksmas == "*")
+                    {
+                        Console.WriteLine("{0} * {1} = {2}", sk1, sk2, sk1 * sk2);
+                    }
+                }
+
+                Console.WriteLine();
+
+                bool toliau2 = true;
+                while (toliau2)
+                {
+                    Console.WriteLine("Ar nori skaičiuoti vėl? (Y/N)");
+                    string dar = Console.ReadLine();
+
+                    if (dar == "Y")
+                    {
+                        Console.WriteLine();
+                        toliau2 = false;
+                    }
+                    else if (dar == "N")
+                    {
+                        Console.WriteLine();
+                        toliau2 = false;
+                        toliau = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nesupratau.");
+                        Console.WriteLine();
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("-------------");
+            Console.WriteLine();
+
+        }
+
+        //Skaičiaus spėliojimo žaidimas. Vykdomas iki kol skaičius atspėjamas 5 kartus.
+        public static void SpeliojimoZaidimas()
+        {
+            int laimejimai = 0;
+            Random rnd = new Random();
+            int sk, spejimas;
+
+            while (laimejimai != 5)
+            {
+                sk = rnd.Next(1, 10);
+                spejimas = 0;
+
+                Console.WriteLine("Aš sugalvojau skaičių nuo 1 iki 10.");
+                Console.WriteLine("Spėkite skaičių!");
+
+                while (sk != spejimas)
+                {
+                    spejimas = int.Parse(Console.ReadLine());
+
+                    if (sk != spejimas)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Nepataikėte. Bandykite dar kartą.");
+                    }
+                }
+
+                laimejimai++;
+                Console.WriteLine();
+                Console.WriteLine("Teisingai! Jau turite {0} teisingus spėjimus", laimejimai);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Užteks žaisti.");
+            Console.WriteLine();
+            Console.WriteLine("-------------");
+            Console.WriteLine();
+        }
+
+        //Funkcija išridena
+        public static void Kauliukas()
+        {
+            Random rnd = new Random();
+            int suma = 0;
+
+            Console.WriteLine("Kokį kauliuką naudosite? (Su kiek daugiausiai taškų?)");
+            int maxTasku = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Kiek kauliukų mesite?");
+            int kiek = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Kauliukai ridenami...");
+            Console.WriteLine();
+
+            for (int i = 0; i < kiek; i++)
+            {
+                suma += rnd.Next(1, maxTasku);
+            }
+            Console.WriteLine("Išridenti {0} taškai.", suma);
+            Console.WriteLine();
+            Console.WriteLine("-------------");
+            Console.WriteLine();
+        }
+
+        //Kavos aparatas funkcija gali pasakyti aparato uždarbį arba parduoti kavos. Aparatas atiduoda grąžą.
+        public static void KavosAparatas()
+        {
+            float kava = 3f;
+            float vanduo = 5f;
+            int uzdarbis = 0;
+            bool toliau = true;
+            string funkcija;
+
+            //kavos aparatas dirba tol, kol nepasakoma, kad nebenorima daugiau funkcijų
+            while (toliau)
+            {
+                float idetaPinigu = 0f;
+                int kaina = 0;
+                float kiekKavos = 0f;
+                float kiekVandens = 0f;
+                string dydis = "";
+
+                Console.WriteLine("Pasirinkite kavos aparato funkciją.");
+                Console.WriteLine("Jei norite kavos, rašykite - Kava");
+                Console.WriteLine("Jei norite sužinote, kiek aparatas yra uždirbęs pinigų, rašykite - Pinigai");
+
+                funkcija = Console.ReadLine();
+
+                //Kavos tiekimas
+                if (funkcija == "Kava")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Kokio kavos puodelio norėtumėte? S, M ar L?");
+                    dydis = Console.ReadLine();
+
+                    switch (dydis)
+                    {
+                        case "S":
+                            kaina = 1;
+                            kiekKavos = 0.5f;
+                            kiekVandens = 0.25f;
+                            break;
+                        case "M":
+                            kaina = 3;
+                            kiekKavos = 0.75f;
+                            kiekVandens = 0.5f;
+                            break;
+                        case "L":
+                            kaina = 5;
+                            kiekKavos = 1f;
+                            kiekVandens = 0.75f;
+                            break;
+                        default:
+                            Console.WriteLine("Tokio dydžio nėra.");
+                            Console.WriteLine();
+                            break;
+                    }
+
+                    if (kaina != 0)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Jūsų kava kainuoja {0}EUR. Galite dėti dalimis.", kaina);
+
+                        //Pinigų skaičiavimas pasirinktam puodeliui
+                        while (idetaPinigu < kaina)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Nepakankamas balansas. Trūksta {0}EUR. Prašome įdėti pinigų.", kaina - idetaPinigu);
+                            idetaPinigu += float.Parse(Console.ReadLine());
+                        }
+
+                        //Grąža duodama tik jei yra ką duoti.
+                        if (idetaPinigu > kaina)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("Jums liko {0}EUR. Pasiimkite grąžą.", idetaPinigu - kaina);
+                        }
+
+                        kava -= kiekKavos;
+                        vanduo -= kiekVandens;
+                        uzdarbis += kaina;
+
+                        Console.WriteLine();
+                        Console.WriteLine(dydis + " kava paruošta!");
+
+                        //Tikrinama, ar užtenka vandens ir kavos aparate
+                        while (kava < 1 || vanduo < 0.75)
+                        {
+                            string papildymas = null;
+                            string kasBaiges = null;
+
+                            if (kava < 1 && vanduo < 0.75)
+                            {
+                                kasBaiges = "kava ir vanduo.";
+                            }
+                            else if (kava < 1)
+                            {
+                                kasBaiges = "kava";
+                            }
+                            else
+                            {
+                                kasBaiges = "vanduo";
+                            }
+
+                            Console.WriteLine();
+                            Console.WriteLine("Pasibaigė {0}. Prašome papildyti.", kasBaiges);
+                            Console.WriteLine("Norint papildyti kavos, rašykite - Kava");
+                            Console.WriteLine("Norint papildyti vandens, rašykite - Vanduo");
+
+                            papildymas = Console.ReadLine();
+
+                            if (papildymas == "Kava")
+                            {
+                                kava = 3f;
+                                Console.WriteLine();
+                                Console.WriteLine("Ačiū. Kavos vėl yra.");
+                            }
+                            else if (papildymas == "Vanduo")
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Ačiū. Vandens vėl yra.");
+                                vanduo = 5f;
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Nesupratau");
+                            }
+                        }
+                    }
+                }
+                //Pasakoma, kiek pinigų uždirbta per programos veikimo laiką.
+                else if (funkcija == "Pinigai")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Kavos aparatas uždirbo {0}EUR.", uzdarbis);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Neteisinga funkcija.");
+                }
+
+                bool toliau2 = true;
+                while (toliau2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Ar norite atlikti kitą funkciją? Y/N?");
+
+                    string dar = Console.ReadLine();
+
+                    if (dar == "Y")
+                    {
+                        Console.WriteLine();
+                        toliau2 = false;
+                    }
+                    else if (dar == "N")
+                    {
+                        Console.WriteLine();
+                        toliau2 = false;
+                        toliau = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nesupratau.");
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("-------------");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 5 paskaita uzduotys/Program.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uzduotys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Pasirinkimas();
+            }
+            Console.ReadLine();
+        }
+        public static void Pasirinkimas()
+        {
+            //Cia aprasykite kintamuosius
+            int a = 3, b = 6, c = 19;
+            string vardas = "Kamile";
+            float kaina = 0f, vidurkis = 8.4f;
+
+            Console.WriteLine("Pasirinkite užduotį (1 - 10)");
+            string input = Console.ReadLine();
+            Console.WriteLine("--------------");
+            Console.WriteLine();
+
+            if (input == "1")
+            {
+                Pirma();
+            }
+            else if (input == "2")
+            {
+                Antra();
+            }
+            else if (input == "3")
+            {
+                Trecia();
+            }
+            else if (input == "4")
+            {
+                Ketvirta();
+            }
+            else if (input == "5")
+            {
+                Penkta(vardas);
+            }
+            else if (input == "6")
+            {
+                Sesta();
+            }
+            else if (input == "7")
+            {
+                Septinta(b, c);
+            }
+            else if (input == "8")
+            {
+                Astunta();
+            }
+            else if (input == "9")
+            {
+                Devinta();
+            }
+            else if (input == "10")
+            {
+                Desimta(b);
+            }
+            else if (input == "11")
+            {
+                Vienuolikta();
+            }
+            else
+            {
+                Console.WriteLine("Neteisingas užduoties numeris. Prašome pakartoti pasirinkimą.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------");
+            Console.WriteLine("Noredami testi paspauskite ENTER");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public static void Pirma()
+        {
+            Console.WriteLine("---- #1 ----");
+            Console.WriteLine();
+            Console.WriteLine("Įveskite skaičių.");
+
+            float sk = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (sk == 0)
+            {
+                Console.WriteLine("Skaičius netinkamas.");
+            }
+            else
+            {
+                PakeltiKvadratu(sk);
+            }
+        }
+
+        public static void PakeltiKvadratu(float sk)
+        {
+            Console.WriteLine("{0}² = {1}", sk, sk * sk);
+        }
+
+        public static void Antra()
+        {
+            Console.WriteLine("---- #2 ----");
+            Console.WriteLine();
+            Console.WriteLine("Įveskite sveiką skaičių.");
+
+            int sk = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Įveskite sakinį.");
+
+            string sakinys = Console.ReadLine();
+
+            Console.WriteLine();
+
+            PildykLenta(sk, sakinys);
+        }
+
+        public static void PildykLenta(int kiek, string sakinys)
+        {
+            for (int i = 0; i < kiek; i++)
+            {
+                Console.WriteLine(sakinys);
+            }
+        }
+
+        public static void Trecia()
+        {
+            string ats1, ats2;
+
+            Console.WriteLine("---- #3 ----");
+            Console.WriteLine();
+            Console.WriteLine("Ar padarei namų darbus?");
+
+            ats1 = Console.ReadLine();
+
+            Console.WriteLine();
+
+            if (ats1 == "Taip")
+            {
+                Console.WriteLine("Šaunuolis(-ė)!, imk sausainį!");
+            }
+            else if (ats1 == "Nesigavo")
+            {
+                Console.WriteLine("Ateik, paaiškinsiu...");
+            }
+            else if (ats1 == "Ne")
+            {
+                Console.WriteLine("Kodėl?");
+
+                ats2 = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if (ats2 == "Pamirsau")
+                {
+                    Console.WriteLine("Kitą kartą nepamiršk!");
+                }
+                else if (ats2 == "Tingejau")
+                {
+                    Console.WriteLine("Sėsk 2!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nesuprantu, ką čia murmi??");
+            }
+
+        }
+        public static void Ketvirta()
+        {
+            int sk, sk1, sk2, sk3, sk4;
+
+            Console.WriteLine("---- #4 ----");
+            Console.WriteLine();
+            Console.WriteLine("Įveskite keturženkį skaičių.");
+
+            sk = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            sk4 = sk % 10;
+            sk3 = sk / 10 % 10;
+            sk2 = sk / 100 % 10;
+            sk1 = sk / 1000;
+
+            Console.WriteLine("sk1 = {0}, sk2 = {1}, sk3 = {2}, sk4 = {3}", sk1, sk2, sk3, sk4);
+        }
+        public static void Penkta(string vardas)
+        {
+            int i = 0;
+            string ivestasVardas;
+
+            Console.WriteLine("---- #5 ----");
+
+            while (i < 2)
+            {
+                Console.WriteLine();
+
+                if (i == 0)
+                {
+                    Console.WriteLine("1. Koks tavo vardas?");
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine("2. Koks tavo vardas?");
+                }
+
+                ivestasVardas = Console.ReadLine();
+
+                if (ivestasVardas == vardas)
+                {
+                    i++;
+                }
+                else
+                {
+                    i = 0;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Sveika, " + vardas);
+        }
+        public static void Sesta()
+        {
+            Console.WriteLine("---- #6 ----");
+            Console.WriteLine();
+
+            for (int i = 5; i <= 15; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void Septinta(int b, int c)
+        {
+            Console.WriteLine("---- #7 ----");
+            Console.WriteLine();
+            Console.WriteLine("Šie skaičiai nuo {0} iki {1} dalijasi iš 3:", b, c);
+
+            for (int i = b; i <= c; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static void Astunta()
+        {
+            Console.WriteLine("---- #8 ----");
+            Console.WriteLine();
+
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(i);
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+        public static void Devinta()
+        {
+            Console.WriteLine("---- #9 ----");
+            Console.WriteLine();
+
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(j);
+                }
+
+                Console.WriteLine(i);
+            }
+        }
+        public static void Desimta(int b)
+        {
+            Console.WriteLine("---- #10 ----");
+            Console.WriteLine();
+
+            for (int i = b; i <= 30; i++)
+            {
+                Console.Write(i + " dalinasi iš:");
+
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        Console.Write(" " + j);
+                    }
+                }
+
+                Console.Write("\n");
+            }
+        }
+        public static void Vienuolikta()
+        {
+            Console.WriteLine("---- #11 ----");
+            Console.WriteLine();
+
+            bool pirminis;
+
+            Console.Write("1");
+
+            for (int i = 1; i <= 9999; i++)
+            {
+                pirminis = false;
+
+                for (int j = 2; j <= i; j++)
+                {
+                    if (j == i)
+                    {
+                        pirminis = true;
+                    }
+                    else
+                    {
+                        if (i % j == 0)
+                        {
+                            j = i;
+                        }
+                    }
+                }
+
+                if (pirminis)
+                {
+                    Console.Write(", ");
+                    Console.Write(i);
+                }
+            }
+        }
+    }
+}
