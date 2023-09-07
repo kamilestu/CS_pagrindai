8c14cb66f81295d27e90f82d91018d9487cd26d3
kamilestu <kamilestu@gmail.com>
2023-09-07 16:24:17 +0400

    Pridedamos naujos atliktos užduotys:  - Laikrodis  - Klasės

---
+++ Klases/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klases
+{
+    class Person
+    {
+        protected int age;
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public void SetAge(int n)
+        {
+            age = n;
+        }
+    }
+
+    class Student : Person
+    {
+        public void GoToClasses()
+        {
+            Console.WriteLine("I'm going to class.");
+        }
+
+        public void ShowAge()
+        {
+            Console.WriteLine("My age is: {0} years old.", age);
+        }
+    }
+
+    class Teacher : Person
+    {
+        private string subject;
+
+        public void Explain()
+        {
+            Console.WriteLine("Explanation begins.");
+        }
+    }
+
+    class StudentAndTeacherTest
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Greet();
+
+            Student student = new Student();
+
+            student.SetAge(21);
+            student.Greet();
+            student.ShowAge();
+
+            Teacher teacher = new Teacher();
+            teacher.SetAge(30);
+            teacher.Greet();
+            teacher.Explain();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laikrodis/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laikrodis
+{
+    class Program
+    {
+        const int maxMinutes = 60;
+        const int maxValandos = 12;
+
+        static void Main(string[] args)
+        {
+            int valandos, minutes;
+            string valandosText, minutesText;
+            float kampas;
+
+            Console.WriteLine("Paeiliui iveskite, kiek valandu ir minuciu rodo laikrodis (sveikaisias skaiciais):");
+            valandosText = Console.ReadLine();
+            minutesText = Console.ReadLine();
+
+            Console.WriteLine();
+
+            if (int.TryParse(valandosText, out valandos) == false || int.TryParse(minutesText, out minutes) == false)
+            {
+                Console.WriteLine("Valandos ir minutes gali buti tik sveikieji skaiciai. Jusu ivesti duomenys neteisingi.");
+            }
+            else if (valandos > maxValandos * 2)
+            {
+                Console.WriteLine("Ivestas valandu skaicius yra per didelis. Paroje yra tik 24 valandos.");
+            }
+            else if (minutes > maxMinutes)
+            {
+                Console.WriteLine("Ivestas minuciu skaicius yra per didelis. Valanda sudaro 60 minuciu.");
+            }
+            else
+            {
+                if (valandos == maxValandos || valandos == maxValandos * 2) { valandos = 0; }
+                if (valandos > maxValandos) { valandos = valandos % maxValandos; }
+                if (minutes == maxMinutes) { minutes = 0; }
+
+                kampas = SkaiciuokKampa(valandos, minutes);
+
+                Console.WriteLine("Mažesnysis kampas tarp laikrodzio rodykliku yra {0} laipsniu.", kampas);
+                Console.ReadLine();
+            }        
+                        
+        }
+
+        public static float SkaiciuokKampa(int valandos, int minutes)
+        {
+            float kampas;
+
+            float valandosMinutemis = (valandos * 5) + (5 * (float)minutes / maxMinutes);
+
+            float atstumasMinutemis = valandosMinutemis - minutes;
+
+            if (atstumasMinutemis < 0) { atstumasMinutemis = atstumasMinutemis * (-1); }
+
+            if (atstumasMinutemis > maxMinutes / 2) { atstumasMinutemis = maxMinutes - atstumasMinutemis; }
+
+            kampas = atstumasMinutemis / maxMinutes * 360;
+
+            return kampas;
+        }
+    }
+}
